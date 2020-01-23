6ad111f560ba42deefc7e24e905c3b2ab7463d8b
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-23 08:16:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  first_element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
