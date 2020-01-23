eb5ebc066abeba57901f094cfc4ae307f357a377
Yuksalbek Rakhimjonov <yuksal97@gmail.com>
2020-01-23 08:19:14 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,42 @@
 def create_an_empty_array
+  array = []
   
 end
 
 def create_an_array
-  
+  four_element_array = ["Real", "Barcelona", "PSG", "Liverpool"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  last_element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  first_element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
+  
+  array[index_number] = element
 
 end
